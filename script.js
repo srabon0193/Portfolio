e7ffcbefb5a317ce5d95aa6271e74f0193ef7620
srabon0193 <skghosh0193@gmail.com>
2023-07-31 23:54:45 +0600

    update

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 // Toggle icon navbar
-let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle("bx-x");
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 };
 
 //Scroll sections
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-  sections.forEach((sec) => {
+  sections.forEach(sec => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 100;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
+      //active navbar lines
+      navLinks.forEach(links => {
+        links.classList.remove('active');
         document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
+          .querySelector('header nav a[href*=' + id + ']')
+          .classList.add('active');
       });
     }
   });
